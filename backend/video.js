b1749f2edbc88f7cd4d9597534880a90a1a43c6d
Samuron <samuronchik@gmail.com>
2016-09-12 15:22:51 +0300

    Sample code to work with firebase DB

---
+++ backend/video.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var firebase = require("firebase");
+var db = firebase.database();
+var videos = db.ref("/videos");
+
+router.post('/', function (req, res) {
+    var newVideo = videos.push();
+    newVideo.set(req.body);
+    req.body.id = newVideo.key;
+    res.send(req.body);
+});
+
+router.get('/', function (req, res) {
+    videos.on("value", snapshot => res.send(snapshot.val()), err => console.log("The read failed: " + err.code));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+var firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "./VideoHustle-026dd1b7d7c8.json",
+  databaseURL: "https://videohustle-d80dd.firebaseio.com/"
+});
+
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+var videosApi = require('./backend/video.js');
+
+app.use(bodyParser.json())
 
 app.use(express.static('dist'));
+
+app.use('/api/videos', videosApi);
 
 app.listen(3000, function () {
   console.log('VideoHustle is waiting you, master, at port 3000');
