e916096fd8ad7b55cc92253043d421bafe9e84cb
sdoomz <sdoomz@gmail.com>
2016-09-14 00:49:10 +0300

    [chat] minor optimisations

--- src/Chat.js
+++ src/Chat.js
@@ -1,50 +1,59 @@
 import React from 'react';
 import firebase from 'firebase';
 import ReactFireMixin from 'reactfire';
-import ChatMessage from './ChatMessage'
+
+const ChatMessage = ({ time, name, message }) => {
+  return <li>{time} {name}: {message}</li>
+};
 
 const Chat = React.createClass({
 
-    mixins: [ReactFireMixin],
+  mixins: [ReactFireMixin],
 
-    getInitialState() {
-        return {
-            messages: [],
-            message: '',
-            chatRef: firebase.database().ref(`/videos/${this.props.videoKey}/chat`)
-        };
-    },
+  getInitialState() {
+    return {
+      messages: [],
+      message: '',
+      chatRef: firebase.database().ref(`/videos/${this.props.videoKey}/chat`)
+    };
+  },
 
-    setValue(propName, value) {
-        this.setState({
-            [propName]: value
-        })
-    },
+  setValue(propName, value) {
+    this.setState({
+      [propName]: value
+    })
+  },
 
-    componentDidMount() {
-        this.bindAsArray(this.state.chatRef, 'messages')
-    },
+  componentDidMount() {
+    this.bindAsArray(this.state.chatRef, 'messages')
+  },
 
-    postMessage(e) {
-        this.state.chatRef.push({ time: new Date().toDateString(), message: this.state.message, name: "Someone" });
-    },
+  postMessage(e) {
+    e.preventDefault();
 
-    render() {
-        function renderChatMessage(m) {
-            return (
-                <li><ChatMessage time ={m.time} name ={m.name}  message={m.message}/></li>
-            )
-        }
-        return (
-            <div>
-                <form onSubmit={ e => this.postMessage(e) }>
-                    <input type="text" placeholder="add message" onChange={ e => this.setValue('message', e.target.value) } />
-                    <button type="submit">Submit</button>
-                </form>
-                <ul>{this.state.messages.map(renderChatMessage) }</ul>
-            </div>
-        )
-    }
+    this.state.chatRef.push({
+      time: new Date().toDateString(),
+      message: this.state.message,
+      name: "Someone"
+    });
+  },
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={ e => this.postMessage(e) }>
+          <input
+            type="text"
+            placeholder="add message"
+            onChange={ e => this.setValue('message', e.target.value) } />
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {this.state.messages.map((message, index) => <ChatMessage key={index} {...message} /> ) }
+        </ul>
+      </div>
+    )
+  }
 });
 
 export default Chat;
