e8635848bdcacb48a71eb46cc966b50e5f11d2aa
Daria Novikova <dashunay@gmail.com>
2016-09-14 21:34:37 +0300

    Initial player state sync

--- src/Video.js
+++ src/Video.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase';
 import ReactFireMixin from 'reactfire';
 import Chat from './Chat';
+import YouTube from 'react-youtube';
 
 const Video = React.createClass({
 
@@ -10,19 +11,63 @@ const Video = React.createClass({
   getInitialState() {
     return {
       key: this.props.params.key,
-      video: null
+      video: null,
+      videoRef: firebase.database().ref(`/videos/${this.props.params.key}`)
     };
   },
 
   componentDidMount() {
-    const videoRef = firebase.database().ref( `/videos/${this.state.key}`);
-    this.bindAsObject(videoRef, 'video')
+    const videoRef = this.state.videoRef;
+    this.bindAsObject(videoRef, 'video');
+  },
+
+  playerReady(event) {
+    var time = this.state.video.time;
+    var state = this.state.video.state;
+    console.log('time:', time);
+    console.log('state:', state);
+    if (time)
+      event.target.seekTo(time, true);
+
+    if (state === 'play') {
+      console.log('start playing');
+      event.target.playVideo();
+    }
+    else {
+      console.log('pause playing');
+      event.target.pauseVideo();
+    }
+  },
+
+  playerPlay(event) {
+    console.log('player play command');
+    var updated = {
+      state: 'play',
+      time: event.target.getCurrentTime()
+    };
+    console.log(updated);
+    this.state.videoRef.update(updated);
+  },
+
+  playerPause(event) {
+    console.log('player pause command');
+    var updated = {
+      state: 'pause',
+      time: event.target.getCurrentTime()
+    };
+    console.log(updated);
+    this.state.videoRef.update(updated);
   },
 
   render() {
     const { video } = this.state;
 
     // console.log( 'key:', this.state.key );
+    const opts = {
+      width: '640',
+      height: '390',
+      frameBorder: '0',
+    };
 
     return (
       <div>
@@ -30,12 +75,12 @@ const Video = React.createClass({
         <div>
           { video ?
             (
-              <iframe id='ytplayer'
-                      type='text/html'
-                      width='640'
-                      height='390'
-                      src={video.link}
-                      frameBorder='0' />
+              <YouTube
+                videoId = {video.link.substr(video.link.lastIndexOf('/') + 1) }
+                opts = {opts}
+                onReady ={this.playerReady}
+                onPlay ={this.playerPlay}
+                onPause={this.playerPause}/>
             ) : null }
         </div>
         <h3>Chat</h3>
